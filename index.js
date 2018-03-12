d8edadcd3b4d33e2005828e8c08a28ae53e73543
awinste <awinste@gmail.com>
2018-03-12 10:53:26 -0500

    send hello world push notification

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const Expo = require('expo-server-sdk');
+
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require ('body-parser');
 const port = process.env.PORT || 9000;
 
 const app = express();
+
+const expo = new Expo();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -14,6 +19,49 @@ app.get('/', (req, res) => {
 
 app.post('/tokens', (req, res) =>{
   console.log('push token: ', req.body);
+  let messages = [];
+  let somePushTokens = [];
+  somePushTokens.push(req.body.token.value);
+  for (let pushToken of somePushTokens) {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
+    messages.push({
+      to: pushToken,
+      sound: 'default',
+      body: 'This is a test notification',
+      data: {
+        withSome: 'data'
+      },
+    })
+  }
+
+  // The Expo push notification service accepts batches of notifications so
+  // that you don't need to send 1000 requests to send 1000 notifications. We
+  // recommend you batch your notifications to reduce the number of requests
+  // and to compress them (notifications with similar content will get
+  // compressed).
+  let chunks = expo.chunkPushNotifications(messages);
+
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let receipts = await expo.sendPushNotificationsAsync(chunk);
+        console.log(receipts);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
   res.status(201).send('token received')
 });
 
